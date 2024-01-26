b541ec71e646327944b72e3bb0431ea34dd2cd1e
Hairunisa <143789547+hairunisa0929@users.noreply.github.com>
2024-01-26 16:42:06 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ["'Roboto'"],
+      },
       colors: {
         primary: "#2457ca",
       },
